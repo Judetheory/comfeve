a4a59c9ffe7d0fbb2a4d9b7d54354e20bf5b707f
Jude Tochukwu Ogbozor <judtheory@gmail.com>
2024-03-27 17:44:18 +0100

    Media Query 645px

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var date = new Date();
 var year = date.getFullYear();
 document.getElementById('curyr').innerHTML = year;
+
+
+let navLinks = document.getElementById("navLinks");
+function showMenu() {
+    navLinks.style.right = "0";
+    
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px"
+    
+}
