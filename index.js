bcdc11cf3adb31f8e45f77a13c3cdf5093c7fbcf
Jude Tochukwu Ogbozor <judtheory@gmail.com>
2024-04-05 15:31:48 +0100

    Client logo

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+/* footer date script*/ 
+
 var date = new Date();
 var year = date.getFullYear();
 document.getElementById('curyr').innerHTML = year;
 
-
+/**Menu Icon Show on media query */
 let navLinks = document.getElementById("navLinks");
 function showMenu() {
     navLinks.style.right = "0";
@@ -13,3 +15,8 @@ function hideMenu() {
     navLinks.style.right = "-200px"
     
 }
+
+/**Duplication of client logo */
+
+let copy = document.querySelector(".client-slide").cloneNode(true);
+document.querySelector(".clients").appendChild(copy);
